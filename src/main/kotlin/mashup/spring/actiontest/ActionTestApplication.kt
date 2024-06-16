7b4d0a94e32fa40f14c9ec60ca0d2700ca69f75a
Diger <kitten.diger@gmail.com>
2024-06-16 14:59:10 +0900

    test : 액션 테스트

--- src/main/kotlin/mashup/spring/actiontest/ActionTestApplication.kt
+++ src/main/kotlin/mashup/spring/actiontest/ActionTestApplication.kt
@@ -11,5 +11,8 @@ fun main(args: Array<String>) {
     println("액션 테스트")
     println("액션 테스트")
     println("액션 테스트")
+    println("액션 테스트")
+    println("액션 테스트")
+    println("액션 테스트")
     runApplication<ActionTestApplication>(*args)
 }
