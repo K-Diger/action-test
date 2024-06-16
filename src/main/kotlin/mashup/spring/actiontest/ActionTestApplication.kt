d81b88e938c239c6e625594ff25b92b393dc98f8
Diger <kitten.diger@gmail.com>
2024-06-16 14:33:17 +0900

    init

---
+++ src/main/kotlin/mashup/spring/actiontest/ActionTestApplication.kt
@@ -0,0 +1,11 @@
+package mashup.spring.actiontest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActionTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<ActionTestApplication>(*args)
+}
---
+++ src/test/kotlin/mashup/spring/actiontest/ActionTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package mashup.spring.actiontest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
